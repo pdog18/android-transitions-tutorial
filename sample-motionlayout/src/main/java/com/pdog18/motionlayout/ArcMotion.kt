33237757d5c9f282e80a91c598d7758c61759454
pdog <pdog@qq.com>
2018-12-29 20:09:15 +0800

    add with viewpager

---
+++ sample-motionlayout/src/main/java/com/pdog18/motionlayout/ArcMotion.kt
@@ -0,0 +1,12 @@
+package com.pdog18.motionlayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ArcMotion : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_arc_motion)
+    }
+}
---
+++ sample-motionlayout/src/main/java/com/pdog18/motionlayout/WithViewPager.kt
@@ -0,0 +1,47 @@
+package com.pdog18.motionlayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.viewpager.widget.PagerAdapter
+import androidx.viewpager.widget.ViewPager
+import kotlinx.android.synthetic.main.activity_with_view_pager.*
+
+class WithViewPager : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_with_view_pager)
+
+        view_pager.adapter = object : PagerAdapter() {
+            override fun getCount() = 2
+
+            override fun destroyItem(container: ViewGroup, position: Int, any: Any) {
+                container.removeView(any as View)
+            }
+
+            override fun instantiateItem(container: ViewGroup, position: Int): Any {
+                val view = ImageView(container.context)
+                view.setImageResource(R.mipmap.ic_launcher)
+                container.addView(view)
+                return view
+            }
+
+            override fun isViewFromObject(view: View, any: Any) = view == any
+        }
+
+        view_pager.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
+            override fun onPageScrollStateChanged(state: Int) {
+            }
+
+            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
+                motion_layout_with_view_pager.progress = (position + positionOffset)
+            }
+
+            override fun onPageSelected(position: Int) {
+            }
+        })
+    }
+}
